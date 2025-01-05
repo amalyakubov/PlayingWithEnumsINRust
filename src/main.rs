d635781d0c4acd383c50117790d646d26bd76a32
Amal Yakubov <amal.yakubov7777@gmail.com>
2025-01-05 17:32:45 +0100

    Add files

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::process::{self, Command};
+
+enum GameCommand {
+    Hello(String),
+    Say(String),
+    Goodbye(String),
+    Quit,
+}
+
+fn process_command(command: GameCommand) {
+    match command {
+        GameCommand::Hello(name) => println!("Hi, {}", name),
+        GameCommand::Quit => println!("You quit the game."),
+        GameCommand::Goodbye(name) => println!("Goodbye, {}", name),
+        GameCommand::Say(message) => println!("You said: {}", message),
+    }
+}
+
+fn main() {
+    let cmd1 = GameCommand::Hello(String::from("Sara"));
+    let cmd2 = GameCommand::Quit;
+    let cmd3 = GameCommand::Goodbye(String::from("Alice"));
+    let cmd4 = GameCommand::Goodbye(String::from("Bob"));
+    let vector: Vec<GameCommand> = vec![cmd1, cmd2, cmd3, cmd4];
+    for command in vector {
+        process_command(command);
+    }
+}
